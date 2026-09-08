494b0a3095f6109dd2aa701e2e6b15136634b022
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-09-08 15:36:16 +0330

    feat: add CMS password change settings

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { TrackingPage } from './pages/TrackingPage';
 import { ContactPage } from './pages/ContactPage';
 import { AdminPage } from './pages/AdminPage';
 import { AdminSecureDownloads } from './pages/AdminSecureDownloads';
+import { AdminPasswordSettings } from './pages/AdminPasswordSettings';
 
 const isTrackingHash = () => window.location.hash.startsWith('#tracking');
 const isContactHash = () => window.location.hash.startsWith('#contact');
@@ -41,6 +42,7 @@ export default function App() {
       <>
         <AdminPage />
         <AdminSecureDownloads />
+        <AdminPasswordSettings />
       </>
     );
   }
