0d818e222c68b24be02c81dadb31d85274613465
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-09-20 23:51:23 +0330

    feat: bundle Persian fonts locally for domestic-only operation

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,13 @@
+import '@fontsource/vazirmatn/400.css';
+import '@fontsource/vazirmatn/500.css';
+import '@fontsource/vazirmatn/600.css';
+import '@fontsource/vazirmatn/700.css';
+import '@fontsource/vazirmatn/800.css';
+import '@fontsource/estedad/400.css';
+import '@fontsource/estedad/500.css';
+import '@fontsource/estedad/600.css';
+import '@fontsource/estedad/700.css';
+import '@fontsource/estedad/800.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
